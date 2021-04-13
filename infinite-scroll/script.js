633507292fcf97052d35716360031ac4806c6316
Loi Pham <loitpham@gmail.com>
2021-04-13 00:37:50 -0700

    Tested the Unsplash API to fetch some test images.

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,1 +1,17 @@
-console.log('testing');
+// Unsplash API
+const countPhotos = 10;
+const apiKey = 'oU0m6w6VS_mGxM_uSkrY6nMV48wKpVaOekvgF-phZvI'; // Deactivated after project completed
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${countPhotos}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+
+    }
+}
+
+getPhotos();
